4e9b4aa98bca56c0a52d2f6ac09aff3c04b7892a
Achuka Simon Allan <87936175+achuka99@users.noreply.github.com>
2023-01-09 15:23:39 +0300

    Setting up Native Base UI

---
+++ AppContents.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AppContents = () => {
+  return (
+    <View>
+      <Text>AppContents</Text>
+    </View>
+  )
+}
+
+export default AppContents
